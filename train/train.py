57ee26c28e51947a0b554bcc7b8f5924c29bda5b
Sermila <sermila@hotmail.com>
2025-07-18 11:37:19 +0100

    train for pyro25img

--- train/train.py
+++ train/train.py
@@ -8,20 +8,23 @@ os.environ["MASTER_ADDR"] = "localhost"
 os.environ["MASTER_PORT"] = "12355"
 os.environ["LOCAL_RANK"] = "0"
 
-# Set dataset path (COCO format expected)
-dataset_path = "/vol/bitbucket/si324/rf-detr-wildfire/rf-initial-test-data/WildfireSmoke.v1-raw.coco"
+# Set dataset path (COCO format)
+dataset_path = "/vol/bitbucket/si324/rf-detr-wildfire/pyro25img/images"
+
+# Set output directory 
+output_dir = "./output/img_data_train_output"
 
 # Initialize model
 model = RFDETRBase()
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Train the model
 model.train(
     dataset_dir=dataset_path,
-    epochs=20,
+    epochs=50,
     batch_size=4,           
     grad_accum_steps=4,     
     lr=1e-4,
     device="cuda",      
-    ddp=False 
+    ddp=False,
+    output_dir=output_dir
 )
