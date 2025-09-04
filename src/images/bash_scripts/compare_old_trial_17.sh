6bcd4b0fc382e19f810ef06b2564a470cc40acb7
Sermila <sermila@hotmail.com>
2025-09-04 09:16:57 +0100

    trial 17

--- src/images/bash_scripts/compare_old_trial_17.sh
+++ src/images/bash_scripts/compare_old_trial_17.sh
@@ -4,8 +4,8 @@
 #SBATCH --partition=AMD7-A100-T,gpgpu,gpgpuB,a16gpu
 #SBATCH --mail-type=ALL 
 #SBATCH --mail-user=si324 
-#SBATCH --output=/vol/bitbucket/si324/rf-detr-wildfire/src/images/compare_old_trial_10/logs/old_trial_10-%j.out
-#SBATCH --error=/vol/bitbucket/si324/rf-detr-wildfire/src/images/compare_old_trial_10/logs/old_trial_10-%j.err
+#SBATCH --output=/vol/bitbucket/si324/rf-detr-wildfire/src/images/compare_trial_17/logs/trial_17-%j.out
+#SBATCH --error=/vol/bitbucket/si324/rf-detr-wildfire/src/images/compare_trial_17/logs/trial_17-%j.err
  
 export PATH=/vol/bitbucket/${USER}/rf-detr-wildfire/.venv/bin:$PATH
 source /vol/bitbucket/${USER}/rf-detr-wildfire/.venv/bin/activate
@@ -16,7 +16,7 @@ source /vol/cuda/12.0.0/setup.sh
 cd /vol/bitbucket/${USER}/rf-detr-wildfire
 
 # Create logs directory if it doesn't exist
-mkdir -p /vol/bitbucket/${USER}/rf-detr-wildfire/src/images/compare_old_trial_10/logs
+mkdir -p /vol/bitbucket/${USER}/rf-detr-wildfire/src/images/compare_trial_17/logs
 
-python src/images/eval/compare_old_trial_10.py
+python src/images/eval/compare_trial_17.py
 
---
+++ src/images/bash_scripts/final_comparison.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+#SBATCH --mem=32G
+#SBATCH --partition=AMD7-A100-T,gpgpu,gpgpuB,a16gpu
+#SBATCH --mail-type=ALL 
+#SBATCH --mail-user=si324 
+#SBATCH --output=/vol/bitbucket/si324/rf-detr-wildfire/src/images/final_comparison/logs/final_comparison-%j.out
+#SBATCH --error=/vol/bitbucket/si324/rf-detr-wildfire/src/images/final_comparison/logs/final_comparison-%j.err
+ 
+export PATH=/vol/bitbucket/${USER}/rf-detr-wildfire/.venv/bin:$PATH
+source /vol/bitbucket/${USER}/rf-detr-wildfire/.venv/bin/activate
+source /vol/cuda/12.0.0/setup.sh
+/usr/bin/nvidia-smi
+
+# Go to the project folder and run the training script
+cd /vol/bitbucket/${USER}/rf-detr-wildfire
+
+# Create logs directory if it doesn't exist
+mkdir -p /vol/bitbucket/${USER}/rf-detr-wildfire/src/images/final_comparison/logs
+
+python src/images/eval/final_comparison.py
+
